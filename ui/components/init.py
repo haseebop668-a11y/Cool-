37b292334ca6961cd8a9223f35c3672f86816c38
The creator <haseebop668@gmail.com>
2026-07-20 23:23:16 +0500

    Create init.py

---
+++ ui/components/init.py
@@ -0,0 +1,8 @@
+"""Reusable UI components module."""
+from .buttons import primary_action
+from .cards import render_card
+from .metrics import display_kpi
+from .status_badges import render_badge
+
+__all__ = ["primary_action", "render_card", "display_kpi", "render_badge"]
+
